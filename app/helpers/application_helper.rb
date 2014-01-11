53f46b59a303e6f70a266cba5369dbe83bc93e35
Ash Tyndall <git@atyndall.net>
2014-01-11 19:28:58 +0800

    Update browser support message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,17 @@
 module ApplicationHelper
+
   def old_browser? (request)
     ua = UserAgent.parse(request.user_agent)
 
     browser = Struct.new(:browser, :version)
     supported= [
-        browser.new('Safari', '5.1.7'),
-        browser.new('Android', '534.30'),
-        browser.new('Chrome', '18.0.1025.166'),
-        browser.new('Firefox', '15'),
-        browser.new('Opera', '15'),
-        browser.new('Internet Explorer', '9'),
+        browser.new('Safari', '4.0'),
+        browser.new('Android', '4.0'),
+        browser.new('Blackberry', '7.0'),
+        browser.new('Chrome', '18.0'),
+        browser.new('Firefox', '16.0'),
+        browser.new('Opera', '15.0'),
+        browser.new('Internet Explorer', '9.0')
     ]
 
     supported.none? { |browser| ua >= browser }
