93be9b1eeac7c3b88e94e0b53e8059e660644014
Ash Tyndall <git@atyndall.net>
2014-01-11 19:33:51 +0800

    Mild formatting tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
         browser.new('Safari', '4.0'),
         browser.new('Android', '4.0'),
         browser.new('Blackberry', '7.0'),
-        browser.new('Chrome', '18.0'),
+        browser.new('Chrome', '999'),
         browser.new('Firefox', '16.0'),
         browser.new('Opera', '15.0'),
         browser.new('Internet Explorer', '9.0')
