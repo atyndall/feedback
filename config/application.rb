12a881ce46b281eb4546019b2e0c43cd1991ffa3
Ash Tyndall <git@atyndall.net>
2014-01-12 18:49:01 +0800

    fix iframe embedding

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Feedback
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
